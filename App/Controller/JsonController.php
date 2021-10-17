9c82b1b6736460e6604eba7d43190680174c3b34
Clementine <clementine.fouque@gmail.com>
2021-10-17 20:11:51 +0200

    adding a class to read CO2.json

--- App/Controller/JsonController.php
+++ App/Controller/JsonController.php
@@ -1,34 +1,44 @@
 <?php
 
-$jsonCo2File = json_decode(file_get_contents("Data/co2.json"), true);
-// $jsonCo2File = Array d'array
-foreach($jsonCo2File as $key=>$array){
-    switch ($key){
-      case "plastiques":
-        foreach ($array as $key=>$plastic){
-          echo $key;
-        }
-        break;
-      case "papier":
-        echo $key;
-        break;
-      case "organique":
-        echo $key;
-        break;
-      case "verre":
-        echo $key;
-        break;
-      case "metaux":
-        echo $key;
-        break;
-      case "autre":
-        echo $key;
-        break;
-      default:
-      echo "default";
+
+namespace App\Controller;
+
+class JsonController {
+
+  function getCo2File(){
+
+  $jsonCo2File = json_decode(file_get_contents("Data/co2.json"), true);
+  $co2Array = [];
+
+  foreach($jsonCo2File as $key=>$array){
+      switch ($key){
+        case "plastiques":
+          foreach ($array as $key=>$plastic){
+            switch ($key){
+              default:
+                $co2Array += [
+                  "{$key}Incineration" => $plastic['incineration'],
+                  "{$key}Recycled" => $plastic['recyclage']
+                  ];
+            }
+          }
+          break;
+        default:
+          if(!is_null($array['incineration'])){
+            $co2Array += ["{$key}Incineration" => $array['incineration']];
+          };
+          if($array['recyclage']){
+            $co2Array += ["{$key}Recycled" => $array['recyclage']];
+          };
+          if(!is_null($array['compostage'])){
+            $co2Array += ["{$key}Composted" => $array['compostage']];
+          };
+          break;
+      }
     }
+
+    return $co2Array;
+
   }
 
-  $jsonDataFile = json_decode(file_get_contents("Data/data.json"), true);
-
-
+}
--- App/index.php
+++ App/index.php
@@ -4,12 +4,17 @@ require_once 'autoload.php';
 
 use App\Model\Waste\Paper;
 use App\Model\WasteTreatment\GlassRecycler;
+use App\Controller\JsonController;
+
 
 echo 'Bonjour et bienvenue !';
 
-$testWaste = new Paper(12,12,12);
-var_dump($testWaste);
-$testIncinerator = new GlassRecycler(23);
-var_dump($testIncinerator);
-$testIncinerator->recycleWaste($testWaste);
-var_dump($testIncinerator);
+// $testWaste = new Paper(12,12,12);
+// var_dump($testWaste);
+// $testIncinerator = new GlassRecycler(23);
+// var_dump($testIncinerator);
+// $testIncinerator->recycleWaste($testWaste);
+// var_dump($testIncinerator);
+
+$hello = new JsonController();
+var_dump($hello->getCo2File());
